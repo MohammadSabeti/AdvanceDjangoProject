35cf937399cb37a0433b4438fa8caa60129bb10d
MohammadSabeti <mohammadi.tik@gmail.com>
2025-11-29 21:35:49 +0330

    feat: set up initial project structure
    - Create 'accounts' and 'blog' Django apps
    - Add Dockerfile and docker-compose.yml
    - Configure containerized development environment

--- BlogProject/core/settings.py
+++ BlogProject/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,23 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0jewy6%#%mb4309%bkm@djtvca^o)bhpjdjcnek94vj1@8a*=$"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config("DB_PORT", cast=int),
+    }
+}
 
 # Application definition
 
@@ -37,6 +48,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'accounts.apps.AccountsConfig',
+    "blog.apps.BlogConfig",
 ]
 
 MIDDLEWARE = [
